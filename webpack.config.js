317644c5498c9a4bfffdee730e6721f56770f77b
Moti Wu <moti.wu@cloud-interactive.com>
2023-07-20 09:42:02 +0800

    Init Webpack, React

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+  },
+};
